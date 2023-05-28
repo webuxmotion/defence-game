0bf762c9fd0332c63d91e4b1313c7d8b556e2519
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2023-05-28 10:39:41 +0300

    Projectile

--- index.js
+++ index.js
@@ -3,28 +3,6 @@ const c = canvas.getContext('2d');
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
-class Player {
-  constructor({ x, y, radius, color }) {
-    this.x = x;
-    this.y = y;
-    this.radius = radius;
-    this.color = color;
-  }
-
-  draw() {
-    c.beginPath();
-    c.arc(
-      this.x,
-      this.y,
-      this.radius,
-      0,
-      Math.PI * 2
-    );
-    c.fillStyle = this.color;
-    c.fill();
-  }
-}
-
 const player = new Player({
   x: canvas.width / 2, 
   y: canvas.height / 2,
@@ -32,4 +10,35 @@ const player = new Player({
   color: "blue"
 });
 
-player.draw();
+const projectiles = [];
+
+const fps = new Fps({ canvasWidth: canvas.width });
+
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, canvas.width, canvas.height);
+
+  projectiles.forEach((projectile) => {
+    projectile.update();
+  })
+  player.draw();
+  fps.update({ c });
+}
+
+addEventListener('click', ({ clientX, clientY }) => {
+  const angle = Math.atan2(clientY - player.y, clientX - player.x);
+  const x = Math.cos(angle) * 5;
+  const y = Math.sin(angle) * 5;
+  projectiles.push(new Projectile({
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    radius: 5,
+    velocity: {
+      x,
+      y
+    },
+    color: 'red'
+  }))
+});
+
+animate();
